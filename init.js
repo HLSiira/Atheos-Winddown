8b63d9a73d3fa0511e2615ad9b1f354905445886
liam <liam@siira.us>
2020-09-05 00:58:03 -0400

    Cleaner Common helper functions

--- init.js
+++ init.js
@@ -1,9 +1,9 @@
 //////////////////////////////////////////////////////////////////////////////80
-// SassCompiler
+// WindDown
 //////////////////////////////////////////////////////////////////////////////80
 // Copyright (c) Atheos & Liam Siira (Atheos.io), distributed as-is and without
-// warranty under the modified License: MIT - Hippocratic 1.2: firstdonoharm.dev
-// See [root]/license.md for more. This information must remain intact.
+// warranty under the MIT License. See [root]/LICENSE.md for more.
+// This information must remain intact.
 //////////////////////////////////////////////////////////////////////////////80
 // Description: 
 // A reminder plugin to encourage users to stay healthy, inspired by the VS Code
